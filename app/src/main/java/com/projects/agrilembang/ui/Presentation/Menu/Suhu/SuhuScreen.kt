ef3b7e4f8089cce79d19676b50bce1b140b48f29
Adrico Alexander <alexanderprb2003@gmail.com>
2024-10-08 11:50:33 +0700

    Menambahkan Chart untuk TemperatureChart

--- app/src/main/java/com/projects/agrilembang/ui/Presentation/Menu/Suhu/SuhuScreen.kt
+++ app/src/main/java/com/projects/agrilembang/ui/Presentation/Menu/Suhu/SuhuScreen.kt
@@ -15,15 +15,21 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.projects.agrilembang.R
+import com.projects.agrilembang.firebase.TemperatureViewModel
+import com.projects.agrilembang.ui.Components.Chart.TemperatureChart
 import com.projects.agrilembang.ui.theme.intersemibold
 
 @Composable
 fun SuhuScreen(
+    viewModel: TemperatureViewModel = viewModel()
 ) {
+    val temperature by viewModel.sensorTemps.observeAsState(emptyList())
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(10.dp),
@@ -47,14 +53,19 @@ fun SuhuScreen(
                         .fillMaxSize()
                 ) {
                     Text(
+                        text = "Data Seluruh Sensor",
                         fontSize = 17.sp,
                         fontFamily = intersemibold,
                     )
                     Text(
+                        text = "Data Realtime Per 3 Jam",
                         fontSize = 12.sp,
                         fontFamily = intersemibold,
                         color = Color.Gray
                     )
+                    TemperatureChart(
+                        context = LocalContext.current,
+                        temperatures = temperature)
                 }
             }
     }
